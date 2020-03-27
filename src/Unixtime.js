1fd50ca060d2c1938615cfd2af768385d2e00749
James Speake <james.speake@goodthingsfoundation.org>
2020-03-27 12:11:28 +0000

    no message

---
+++ src/Unixtime.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+class Unixtime extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { date: new Date() }
+    }
+
+    render() {
+        return (
+            <div>
+            <h1>{ Math.round(this.state.date.getTime() / 1000) }</h1>
+            <p>{ this.state.date.toString() }</p>
+            </div>
+        );
+    }
+}
+
+export default Unixtime;
